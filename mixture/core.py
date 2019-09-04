553c93b24b51049d266233e1297dd1514b9cd38d
Sylvain MARIE <sylvain.marie@se.com>
2019-09-04 18:23:02 +0200

    Initial version with just a core mixin mechanism, inspired from https://github.com/yupeng0921/pymixin

---
+++ mixture/core.py
@@ -0,0 +1,178 @@
+#  Authors: Sylvain Marie <sylvain.marie@se.com>
+#
+#  Copyright (c) Schneider Electric Industries, 2019. All right reserved.
+from warnings import warn
+
+
+try:  # python 3.5+
+    from typing import Optional, Set, List, Callable, Dict
+except ImportError:
+    pass
+
+
+FROM_MIXINS_TAG = '__from_mixins__'
+"""Attribute set to classes to remember which members where copied from mixins"""
+
+
+class MixinContainsInitWarning(UserWarning):
+    pass
+
+
+class MixinNotRegisterableWarning(UserWarning):
+    pass
+
+
+def apply_mixins(*mixin_classes):
+    """
+    Decorator to apply a list of mix-in classes in order, to the decorated class.
+    The left-most class will be applied last, so as to get the same intuitive behaviour than explicit inheritance in
+    the same order.
+
+    Classes are
+
+    :param mixin_classes:
+    :param cls: the decorated class
+    :return:
+    """
+    def _effectively_decorate(orig_cls):
+
+        # First gather everything that has to be done
+        to_copy = dict()
+        for mixin_class in reversed(mixin_classes):
+            # display a warning if the mixin class contains an __init__
+            if '__init__' in mixin_class.__dict__:
+                warn("Mixin class '%' contains an explicit `__init__` method. This is highly NOT recommended.",
+                     MixinContainsInitWarning)
+
+            # list all methods that should be copied
+            res = list_all_members_to_copy(mixin_class, orig_cls)
+            to_copy.update(res)
+
+        # Now perform copy or create a new type
+        if issubclass(orig_cls, object):
+            # --- new-style class, no need to create a new type
+
+            # copy all members
+            for m_name, member in to_copy.items():
+                setattr(orig_cls, m_name, member)
+
+            # fill the __from_mixins__ field with the list of names copied
+            setattr(orig_cls, FROM_MIXINS_TAG, tuple(to_copy.keys()))
+
+            out_cls = orig_cls
+
+        else:
+            # --- old-style class, need to create a new class
+
+            # with the same class type, class name and class parents
+            orig_cls_type = type(orig_cls)
+            orig_cls_name = orig_cls.__name__
+            orig_cls_bases = orig_cls.__bases__
+
+            # but with members that also include the new ones
+            # --original
+            orig_vars = copy_cls_vars(orig_cls)
+            # --new ones
+            for m_name, member in to_copy.items():
+                orig_vars[m_name] = member
+            # --FROM_MIXINS_TAG
+            orig_vars[FROM_MIXINS_TAG] = tuple(to_copy.keys())
+
+            out_cls = orig_cls_type(orig_cls_name, orig_cls_bases, orig_vars)
+
+        # register the output class as a subclass of all mixins that support it (python ABC mechanism)
+        for mixin_class in reversed(mixin_classes):
+            try:
+                mixin_class.register(out_cls)
+            except AttributeError:
+                warn(
+                    "Mixin class '%s' does not seem to be an ABC so it can not be registered as the virtual parent of "
+                    "class '%s'. As a result issubclass and isinstance will result `False`. You probably wish your "
+                    "mixin class to inherit from `ABC` or use meta `ABCMeta` to fix this", MixinNotRegisterableWarning)
+
+        return out_cls
+
+    return _effectively_decorate
+
+
+def list_all_members_to_copy(source_cls, dest_cls):
+    # type: (...) -> Dict[str, Callable]
+    """
+    Returns a set containing all members from source class `source_cls` that should be copied to destination class
+    `dest_cls`. These are all members, except for:
+
+     - those that are part of the `__from_mixins__` list
+     - private members whose names start with '_'
+     - members already existing in `dest_cls` itself (not its hierarchy)
+
+    :param source_cls:
+    :param dest_cls:
+    :param force_copy: the names that should be copied in all cases
+    :return:
+    """
+    members_to_copy = dict()
+    force_copy = getattr(dest_cls, FROM_MIXINS_TAG, ())
+
+    for m_name, member in source_cls.__dict__.items():
+        # exclude private members
+        # exclude explicitly overridden members
+        # note: do not use hasattr as we only want to see explicitly overridden
+        should_copy = (m_name in force_copy) \
+                      or ((not m_name.startswith('_')) and (m_name not in dest_cls.__dict__))
+
+        if should_copy:
+            members_to_copy[m_name] = member
+
+    return members_to_copy
+
+
+# def copy_all_members(source_cls, dest_cls, return_copied_names=False, force_copy=()):
+#     # type: (...) -> Optional[Set]
+#     """
+#     Copies all members from source class `source_cls` to destination class `dest_cls`, except for:
+#
+#      - private members whose names start with '_'
+#      - members already existing in `dest_cls` itself (not its hierarchy)
+#
+#     :param source_cls:
+#     :param dest_cls:
+#     :param return_copied_names: if True a set of names copied will be returned
+#     :param force_copy: the names that should be copied in all cases
+#     :return:
+#     """
+#     if return_copied_names:
+#         copied_names = set()
+#
+#     for m_name, member in source_cls.__dict__.items():
+#         # exclude private members
+#         # exclude explicitly overridden members
+#         # note: do not use hasattr as we only want to see explicitly overridden
+#         should_copy = (m_name in force_copy) \
+#                       or (not m_name.startswith('_')) \
+#                       or (m_name not in dest_cls.__dict__)
+#
+#         if should_copy:
+#             setattr(dest_cls, m_name, member)
+#             if return_copied_names:
+#                 copied_names.add(m_name)
+#
+#     if return_copied_names:
+#         return copied_names
+
+
+def copy_cls_vars(cls):
+    """
+    From https://github.com/yupeng0921/pymixin/blob/master/mixin.py
+    :param cls:
+    :return:
+    """
+    cls_vars = cls.__dict__.copy()
+    slots = cls_vars.get('__slots__')
+    if slots is not None:
+        if isinstance(slots, str):
+            slots = [slots]
+        for slots_var in slots:
+            cls_vars.pop(slots_var)
+    cls_vars.pop('__dict__', None)
+    cls_vars.pop('__weakref__', None)
+    return cls_vars
--- mixture/tests/test_main.py
+++ mixture/tests/test_main.py
@@ -1,2 +1,94 @@
-def test_dummy():
-    pass
+import sys
+from abc import ABCMeta
+
+import pytest
+from six import with_metaclass
+
+from mixture import apply_mixins
+from mixture.core import MixinNotRegisterableWarning, MixinContainsInitWarning
+
+
+@pytest.mark.parametrize("old_style_class", [False, True], ids="oldstyle={}".format)
+def test_apply_mixins(old_style_class):
+    """ nominal test with two mixin classes, checks that the overriding order is correct"""
+
+    class DummyMixinA(type):
+        def foo(self, a):
+            return a + 1
+
+    class DummyMixinB(object):
+        def foo(self, a):
+            return a + 2
+
+    if not old_style_class:
+        @apply_mixins(DummyMixinA, DummyMixinB)
+        class MyClass(object):
+            pass
+    else:
+        # create an old-style class
+        if sys.version_info < (3, 0):
+            @apply_mixins(DummyMixinA, DummyMixinB)
+            class MyClass:
+                pass
+        else:
+            # python 3: harder, impossible ?
+            pytest.skip("it is not possible to create an old style class with python 3+")
+
+    # Check that copy worked correctly
+    assert MyClass.__from_mixins__ == ('foo',)
+    if sys.version_info > (3, 0):
+        assert MyClass.foo is DummyMixinA.foo
+    else:
+        assert MyClass.foo.im_func is DummyMixinA.foo.im_func
+
+    # create an object from the class
+    o = MyClass()
+    assert o.foo(1) == 2  # and not 3
+
+
+def test_apply_mixins_warning_none():
+    """Checks that no warning is issued if the mixin class is correct"""
+
+    if sys.version_info >= (3, 0):
+        from abc import ABC
+
+        class DummyMixinAbcWithoutInit(ABC):
+            pass
+
+        with pytest.warns(None) as record:
+            @apply_mixins(DummyMixinAbcWithoutInit)
+            class MyClass:
+                pass
+        assert len(record) == 0
+
+    class DummyMixinAbcWithoutInit(with_metaclass(ABCMeta, object)):
+        pass
+
+    with pytest.warns(None) as record:
+        @apply_mixins(DummyMixinAbcWithoutInit)
+        class MyClass:
+            pass
+    assert len(record) == 0
+
+
+def test_apply_mixins_warning_init():
+    """checks that warning is issued in case an init method is present on the mixin class"""
+    class DummyMixinWithInit(object):
+        def __init__(self):
+            pass
+
+    with pytest.warns(MixinContainsInitWarning, match="contains an explicit `__init__` method"):
+        @apply_mixins(DummyMixinWithInit)
+        class MyClass(object):
+            pass
+
+
+def test_apply_mixins_warning_abc():
+    """Checks that a warning is issued when the mixin class is not an ABC"""
+    class DummyMixinNotAbc(type):
+        pass
+
+    with pytest.warns(MixinNotRegisterableWarning, match="does not seem to be an ABC"):
+        @apply_mixins(DummyMixinNotAbc)
+        class MyClass(object):
+            pass
